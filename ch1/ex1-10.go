92f2b06c0b217028a811866417ef5461da14ffbb
Joakim <joakim.rishaug@finn.no>
2016-04-21 09:16:38 +0200

    added ex1-10

---
+++ ch1/ex1-10.go
@@ -0,0 +1,61 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 17.
+//!+
+
+// Fetchall fetches URLs in parallel and reports their times and sizes.
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+	"sync"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, _ := os.Create("myfile.txt")
+	defer file.Close()
+
+	var mutex = &sync.Mutex{}
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start a goroutine
+	}
+	for range os.Args[1:] {
+		mutex.Lock()
+		file.WriteString(<-ch + "\n") // receive from channel ch and write to file.
+		mutex.Unlock()
+	}
+	file.WriteString(strconv.FormatFloat(time.Since(start).Seconds(), 'f', 6, 64) + "s elapsed\n")
+}
+
+func fetch(url string, ch chan<- string) {
+	if !strings.HasPrefix(url, "http://www."){
+		url = "http://" + url
+	}
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close() // don't leak resources
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
+
+//!-
--- ch1/ex1-3.go
+++ ch1/ex1-3.go
@@ -1,5 +1,36 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"time"
+	"strings"
+	"strconv"
+)
 
+func main() {
+	concatables := make([]string, 900000000)
+	for i := 0; i < 1000; i++{
+		concatables[i] = "abby"
+	}
+
+	stringer := ""
+	start_concat := time.Now()
+	for _, string_at_index := range concatables{
+		stringer += string_at_index
+	}
+	secs_concat := time.Since(start_concat).Seconds()
+	start_join := time.Now()
+	strings.Join(concatables, " ")
+	secs_join := time.Since(start_join).Seconds()
+
+
+
+	fmt.Println("Concat took: " + formatFloat(secs_concat) + " seconds")
+	fmt.Println("Join took: " + formatFloat(secs_join) + " seconds")
+	fmt.Println("Concat took: " + formatFloat((secs_concat/secs_join)*100) + " percent of join's time.")
+
+}
+
+func formatFloat(vari float64)string{
+	return strconv.FormatFloat(vari, 'f', 6, 64)
 }
